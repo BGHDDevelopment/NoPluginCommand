4dcabe63b79cb6a825bd5c434175b10f8c1ebc55
StephanBH <bghdpayments@gmail.com>
2019-11-16 15:37:31 -0800

    Initial commit

---
+++ src/main/java/net/noodles/pl/nopluginscommand/NoPluginsCommand.java
@@ -0,0 +1,37 @@
+package net.noodles.pl.nopluginscommand;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class NoPluginsCommand extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+    @EventHandler
+    public void onCommandUse(PlayerCommandPreprocessEvent event) {
+        Player player = event.getPlayer();
+        List<String> commands = Arrays.asList("pl", "about", "version", "ver", "plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecaft:ver");
+        commands.forEach(all -> {
+         if (event.getMessage().toLowerCase().equalsIgnoreCase("/" + all.toLowerCase())) {
+             event.setCancelled(true);
+         }
+        });
+    }
+
+}
