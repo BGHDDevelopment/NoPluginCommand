4394a1393ee1e8628524ddff5f9ac7fe706769cb
BGHDDevelopment <ceo@bghddevelopment.com>
2022-03-10 09:34:51 -0800

    Version push

--- src/main/java/net/noodles/pl/nopluginscommand/NoPluginsCommand.java
+++ src/main/java/net/noodles/pl/nopluginscommand/NoPluginsCommand.java
@@ -1,6 +1,5 @@
 package net.noodles.pl.nopluginscommand;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -13,15 +12,8 @@ public final class NoPluginsCommand extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         getServer().getPluginManager().registerEvents(this, this);
     }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
 
     @EventHandler
     public void onCommandUse(PlayerCommandPreprocessEvent event) {
